13a0a82986a5dc6fb3d04a6cc701e9e5fe936a7d
denis.sh <denniskitun@gmail.com>
2018-07-20 17:08:03 +0300

    resume providing authentication

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,17 +1,44 @@
+import json
+from calendar import timegm
+from datetime import datetime
+
 from django.http import HttpResponse
 from django.shortcuts import render
 # from rest_framework_jwt.authentication import
+from multilibrary import settings
+from rest_framework import serializers
+from .models import User
+
+class UserSerializer(serializers.Serializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
 
 def hello(request):
     print(request.user.username)
     return HttpResponse('hello_world! %s' %request)
 
 # example custom serializer for
+def jwt_payload_handler(user):
+    return {
+        'user_id': user.pk,
+        'email': user.email,
+        'is_superuser': user.is_superuser,
+        'exp': datetime.utcnow() + settings.JWT_AUTH['JWT_EXPIRATION_DELTA'],
+        'orig_iat': timegm(
+            datetime.utcnow().utctimetuple()
+        )
+    }
+
 def jwt_response_payload_handler(token, user=None, request=None):
-    print('debug is missed(')
-    import os
-    print(os.environ)
+    """ Custom response payload handler.
+
+    This function controlls the custom payload after login or token refresh. This data is returned through the web API.
+    """
+    response_instance = UserSerializer(user).data
+    print(type(response_instance), response_instance)
     return {
         'token': token,
-        'user': dict(user, context={'request': request})
+        'user': response_instance
     }
--- multilibrary/settings.py
+++ multilibrary/settings.py
@@ -111,16 +111,17 @@ JWT_AUTH = {
     'rest_framework_jwt.utils.jwt_decode_handler',
 
     'JWT_PAYLOAD_HANDLER':
-    'rest_framework_jwt.utils.jwt_payload_handler',
-    # 'apps.users.views.jwt_response_payload_handler',
+    # 'rest_framework_jwt.utils.jwt_payload_handler',
+    'apps.users.views.jwt_payload_handler',
 
     'JWT_PAYLOAD_GET_USER_ID_HANDLER':
     'rest_framework_jwt.utils.jwt_get_user_id_from_payload_handler',
 
     'JWT_RESPONSE_PAYLOAD_HANDLER':
-    'rest_framework_jwt.utils.jwt_response_payload_handler',
+    # 'rest_framework_jwt.utils.jwt_response_payload_handler',
+    'apps.users.views.jwt_response_payload_handler',
 
-    'JWT_SECRET_KEY': SECRET_KEY,
+           'JWT_SECRET_KEY': SECRET_KEY,
     'JWT_GET_USER_SECRET_KEY': None,
     'JWT_PUBLIC_KEY': None,
     'JWT_PRIVATE_KEY': None,
